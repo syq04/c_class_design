8096b16f640e05ad0f89dbf0d383307cf6f41bf4
syq <46069887+syq04@users.noreply.github.com>
2024-03-21 18:30:55 +0800

    1

--- class design/main/main.cpp
+++ class design/main/main.cpp
@@ -1,17 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int  W = 4,H = 4;//height,width
+const int  W = 4,H = 4,N=20;//height,width,number of expressman
 
 struct window{
 	int number[W + 1][H + 1];
 	char size[W + 1][H + 1];
 	bool state[W + 1][H + 1];
-}w;
+}win;
 /*	4*4
 *	small   small   small   small
 *	normal  normal  normal  normal
 *	big		big		big		big
 */
+struct expressman {
+	string name;
+}exp[N];
 
 void initialize()
 {
@@ -19,18 +22,18 @@ void initialize()
 	int n = 1;
 	for(int i=1;i<=H;i++)
 		for (int j = 1; j <= W; j++,n++)
-			w.number[i][j] = n;
+			win.number[i][j] = n;
 	//size
 	for (int i = 1; i <=W; i++)
-		w.size[1][i] ='S';
+		win.size[1][i] ='S';
 	for (int i = 1; i <= W; i++)
-		w.size[2][i] = 'N';
+		win.size[2][i] = 'N';
 	for (int i = 1; i <= W; i++)
-		w.size[3][i] ='B';
+		win.size[3][i] ='B';
 	//state
 	for (int i = 1; i <= H; i++)
 		for (int j = 1; j <= W; j++, n++)
-			w.state[i][j] = true;
+			win.state[i][j] = true;
 	return;
 }
 
@@ -40,20 +43,20 @@ void print()//use to test
 	{
 		for (int j = 1; j <= W; j++)
 		{
-			cout << w.number[i][j] << ' ';
+			cout << win.number[i][j] << ' ';
 		}
 		cout << endl;
 	}
 	for (int i = 1; i <= H; i++)
 	{
 		for (int j = 1; j <= W; j++)
-			cout<<w.size[i][j]<<' ';
+			cout<<win.size[i][j]<<' ';
 		cout << endl;
 	}
 	for (int i = 1; i <= H; i++)
 	{
 		for (int j = 1; j <= W; j++)
-			cout << w.state[i][j] << ' ';
+			cout << win.state[i][j] << ' ';
 		cout << endl;
 	}
 	return ;
