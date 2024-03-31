d6dbf0635ed92d92561f87ee092fb3b1f364b9ce
syq <46069887+syq04@users.noreply.github.com>
2024-03-31 13:54:08 +0800

    temp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,31 @@
 #include<bits/stdc++.h>
 //#pragma warning(disable:4996)//use in visual code
 using namespace std;
+
 const int  W = 4,H = 4;//height,width,number of expressman
 int N;
-struct window{
-	int number;
-	string size;
-	bool state;
-}win[W+1][W+1];
-/*	4*4
+/*	windows size
 *	small   small   small   small
 *	normal  normal  normal  normal
 *	big		big		big		big
 */
+
+
+struct window{
+	int number;
+	string size;
+	bool state;
+	string user;
+}win[W+1][W+1];
+
 struct expressman {
 	string name;
 	string phone_number;
 	string password;
 	string company;
-}expm[10000];//maxn
+}expm[10000];
 
-void initialize()
+void initialize()//use to initialize
 {
 	//window----------
 	//number
@@ -53,35 +58,57 @@ void initialize()
 void print()//use to test
 {
 	//windows
-	// for (int i = 1; i <= H; i++)
-	// {
-	// 	for (int j = 1; j <= W; j++)
-	// 	{
-	// 		cout << win[i][j].number << ' ';
-	// 	}
-	// 	cout << endl;
-	// }
-	// for (int i = 1; i <= H; i++)
-	// {
-	// 	for (int j = 1; j <= W; j++)
-	// 		cout<<win[i][j].size<<' ';
-	// 	cout << endl;
-	// }
-	// for (int i = 1; i <= H; i++)
-	// {
-	// 	for (int j = 1; j <= W; j++)
-	// 		cout << win[i][j].state << ' ';
-	// 	cout << endl;
-	// }
-	//epxm
+	for (int i = 1; i <= H; i++)
+	{
+		for (int j = 1; j <= W; j++)
+		{
+			cout << win[i][j].number << ' ';
+		}
+		cout << endl;
+	}
+	for (int i = 1; i <= H; i++)
+	{
+		for (int j = 1; j <= W; j++)
+			cout<<win[i][j].size<<' ';
+		cout << endl;
+	}
+	for (int i = 1; i <= H; i++)
+	{
+		for (int j = 1; j <= W; j++)
+			cout << win[i][j].state << ' ';
+		cout << endl;
+	}
+	//expm
 	for (int i = 1; i <= N; i++)
 		cout<< expm[i].name<<' '<<expm[i].company<<' '<<expm[i].password<<' '<<expm[i].phone_number<<endl;	
 	return ;
 }
 
+void welcome()
+{
+	cout<<"Time:";
+	time_t t = time(nullptr);
+    tm *now = localtime(&t); 
+    cout << put_time(now, "%Y-%m-%d %H:%M:%S") << endl;
+	cout<<"Welcome to ";
+}
+
+void login()
+{
+	cout<<"请快递员输入手机号"<<endl;
+
+}
+
+void delivery()
+{
+	login();
+}
+
 int	 main()
 {
 	initialize();
-	print();//use to test
+	//print();//use to test
+	welcome();
+	delivery();
 	return 0;
 }
