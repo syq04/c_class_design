8d76c86afbaee4b03aa9a22ce52cb935eb9ad803
syq <46069887+syq04@users.noreply.github.com>
2024-03-31 16:05:47 +0800

    V0.1

--- main.cpp
+++ main.cpp
@@ -16,7 +16,8 @@ struct window{
 	int number;
 	string size;
 	bool state;
-	string user;
+	int phone_number;
+	int password;
 }win[W+1][W+1];
 
 struct expressman {
@@ -56,7 +57,7 @@ void initialize()//use to initialize
 	return ;
 }
 
-void print()//use to test
+void print_test()//use to test
 {
 	//windows
 	for (int i = 1; i <= H; i++)
@@ -85,6 +86,161 @@ void print()//use to test
 	return ;
 }
 
+void print_state()
+{
+
+	cout<<"----------------------------------------"<<endl;
+	for (int i = 1; i <= H; i++)
+	{
+		for (int j = 1; j <= W; j++)
+		{
+			
+			cout<<"|";
+			if(win[i][j].number<10)
+				cout<<"0";
+			cout<<win[i][j].number;
+			if( win[i][j].state == true)
+				cout<<"可用  ";
+			else
+				cout<<"不可用";
+		}
+		cout<<'|'<<endl<<"----------------------------------------"<<endl;
+	}
+
+	return;
+}
+
+void password_out(){
+	ofstream outfile;
+	outfile.open("password.out");
+	for (int i = 1; i <= H; i++)
+	{
+		for (int j = 1; j <= W; j++)
+		{
+			if(win[i][j].state==false)
+				outfile<<"|"<<win[i][j].password;
+			else 
+				outfile<<"|"<<"XXXX";
+		}
+		outfile<<"|"<<endl;
+	}
+	outfile.close();
+}
+
+void put_in()
+{
+	cout<<"请选择要存入的窗格号：";
+	int t;
+	cin>>t;
+	if(t>H*W){
+		cout<<"输入错误请重新输入。";
+		Sleep(1000);
+		put_in();
+	}
+	
+	int x,y;
+	for (int i = 1; i <= H; i++)
+		for (int j = 1; j <= W; j++)
+			if(win[i][j].number==t)
+			{
+				x=i,y=j;
+				break;
+			}
+	if(win[x][y].state==false)
+	{
+		cout<<"该窗口暂时不可用，请换一个。"<<endl;
+		Sleep(1000);
+		put_in();
+	}
+	win[x][y].state=false;
+	print_state();
+	cout<<endl<<"请输入顾客手机尾号后4位：";
+	cin>>win[x][y].phone_number;
+
+	default_random_engine e;
+    uniform_int_distribution<int> u(1000,9999);  //左闭右闭区间
+    e.seed(time(0));
+	win[x][y].password=u(e);
+	cout<<endl<<"顾客取件码为:"<<win[x][y].password;
+	password_out();
+	cout<<"存入完成";
+	Sleep(1000);
+	return;
+}
+
+void delivery_login(int n)
+{	
+	system("cls");
+	
+	cout<<expm[n].name<<",您好。"<<endl;
+	cout<<"adsd0";
+	cout<<"您的电话号码是"<<expm[n].phone_number<<endl;
+	cout<<"请输入您的登录密码：";
+	string temp_pass;
+	cin>>temp_pass;
+	if(temp_pass==expm[n].password)
+	{
+		system("cls");
+		print_state();
+		put_in();
+	}
+	else
+	{
+		cout<<"密码错误请重新输入。";
+		Sleep(1500);
+		delivery_login(n);
+	}
+}
+
+void get_out(int x,int y)
+{
+	cout<<endl<<"您的包裹在"<<win[x][y].number<<"号窗口，请输入取件码";
+	int temp;
+	cin>>temp;
+	if(temp!=win[x][y].password)
+	{
+		cout<<endl<<"密码错误请重新输入:";
+		Sleep(1000);
+		get_out(x,y);
+	}
+	else
+	{
+		win[x][y].state=true;
+		win[x][y].phone_number=0;
+		win[x][y].password=0;
+		cout<<endl<<"请取走您的包裹";
+		Sleep(1000);
+	}
+}
+
+void customer()
+{
+	system("cls");
+	cout<<"您好,取件请输入手机尾号后4位：";
+	int num;
+	cin>>num;
+	int l,k;
+	bool flag=false;
+	for (int i = 1; i <= H; i++)
+	{
+		for (int j = 1; j <= W; j++)
+		{
+			if(win[i][j].phone_number==num)
+			{
+				flag=true;
+				l=i,k=j;
+			}
+		}
+	}
+	if(flag==false)
+	{
+		cout<<endl<<"未查询到您的包裹，请重新输入";
+		Sleep(1000);
+		customer();
+	}	
+	get_out(l,k);
+}
+
 void login()
 {		
 	system("cls");
@@ -95,18 +251,22 @@ void login()
     tm *now = localtime(&t); 
     cout << put_time(now, "%Y-%m-%d %H:%M:%S") << endl;
 	cout<<"欢迎使用外卖柜"<<endl;
+	cout<<"当前使用状态："<<endl;
+	print_state();
 	cout<<"顾客按1，骑手按2"<<endl;
 	int identity;
 	cin>>identity;
 
-	if(identity==1)
+	if(identity==2)
 	{		
 		flag=true;
-		delivery();
-		cout<<"D";
+		cout<<"请输入您的快递员编号:";
+		int t;
+		cin>>t;
+		delivery_login(t);
 		return;
 	}
-	if(identity==2)
+	if(identity==1)
 	{
 		flag=true;
 		customer();
@@ -122,20 +282,13 @@ void login()
 	
 }
 
-void delivery()
-{
-	
-}
 
-void customer()
-{
-	
-}
 
 int	 main()
 {
 	initialize();
-	//print();//use to test
-	login();
+	//print_test();
+	while(1)
+		login();
 	return 0;
 }
