f19160dd216dc6565b1fb0ecae03271aff8bf74b
syq <46069887+syq04@users.noreply.github.com>
2024-04-27 18:23:28 +0800

    v1.0

--- main.cpp
+++ main.cpp
@@ -1,62 +1,64 @@
-﻿#include<bits/stdc++.h>
+﻿#include <bits/stdc++.h>
 #include <windows.h>
-//#pragma warning(disable:4996)//use in visual code
 using namespace std;
 
-const int  W = 4, H = 4;//height,width,number of expressman
+const int W = 4, H = 4;
 int N;
-/*	windows size
-*	small   small   small   small
-*	normal  normal  normal  normal
-*	big		big		big		big
-*/
-
-
-struct window {
+struct window
+{
 	int number;
 	string size;
 	bool state;
 	int phone_number;
 	int password;
-	int use_date[10000];
-}win[W + 1][W + 1];
+	struct use_date
+	{
+		int use_time;
+		struct use_time
+		{
+			int in_hour=0;
+			int in_minute=0;
+			int in_sec=0;
+			int out_sec=0;
+			int out_hour=0;
+			int out_minute=0;
+		} use_t[100];
+	} use_date[10000];
+} win[W + 1][W + 1];
 
-struct expressman {
+struct expressman
+{
 	string name;
 	string phone_number;
 	string password;
 	string company;
-	
-}expm[10000];
 
-struct count{
+} expm[10000];
+
+struct count
+{
 	string company;
 	string time[100];
-	int i=1;
-}cnt[6];
+	int i = 1;
+} cnt[6];
 
-int getRand(int min, int max) {
+int getRand(int min, int max)
+{
 	return (rand() % (max - min + 1)) + min;
 }
 
-void initialize()//use to initialize
+void initialize()
 {
-	cnt[1].company="SF";
-	cnt[2].company="ZT";
-	cnt[3].company="YT";
-	cnt[4].company="JD";
-	cnt[5].company="YD";
-
-	for (int i = 1; i <= H; i++)
-		for (int j = 1; j <= W; j++)
-			for(int k=1;k<=9999;k++)
-				win[i][j].use_date[k]=0;
-
+	cnt[1].company = "SF";
+	cnt[2].company = "ZT";
+	cnt[3].company = "YT";
+	cnt[4].company = "JD";
+	cnt[5].company = "YD";
 	int n = 1;
 	for (int i = 1; i <= H; i++)
 		for (int j = 1; j <= W; j++, n++)
 			win[i][j].number = n;
-	//size
+
 	for (int i = 1; i <= W; i++)
 		win[1][i].size = "  Small ";
 	for (int i = 1; i <= W; i++)
@@ -65,11 +67,10 @@ void initialize()//use to initialize
 		win[3][i].size = "   Big  ";
 	for (int i = 1; i <= W; i++)
 		win[4][i].size = "   Big  ";
-	//state
+
 	for (int i = 1; i <= H; i++)
 		for (int j = 1; j <= W; j++, n++)
 			win[i][j].state = true;
-	//expm----
 
 
 	ifstream infile;
@@ -79,12 +80,17 @@ void initialize()//use to initialize
 	for (int i = 1; i <= N; i++)
 		infile >> expm[i].name >> expm[i].company >> expm[i].password >> expm[i].phone_number;
 	infile.close();
+
+	for (int a = 101; a <= 1231; a++)
+		for (int i = 1; i <= H; i++)
+			for (int j = 1; j <= W; j++)
+				win[i][j].use_date[a].use_time = 1;
 	return;
 }
 
-void print_test()//use to test
+void print_test()
 {
-	//windows
+
 	for (int i = 1; i <= H; i++)
 	{
 		for (int j = 1; j <= W; j++)
@@ -106,9 +112,12 @@ void print_test()//use to test
 		cout << endl;
 	}
 
-
 	for (int i = 1; i <= N; i++)
-		cout << expm[i].name << endl << expm[i].company << endl << expm[i].password << endl << expm[i].phone_number << endl << endl;
+		cout << expm[i].name << endl
+			 << expm[i].company << endl
+			 << expm[i].password << endl
+			 << expm[i].phone_number << endl
+			 << endl;
 	return;
 }
 
@@ -137,26 +146,26 @@ void print_state()
 			cout << "|";
 			cout << win[i][j].size;
 		}
-		cout << '|' << endl << "----------------------------------------" << endl;
+		cout << '|' << endl
+			 << "----------------------------------------" << endl;
 	}
 
 	return;
 }
 
-void record(string c,string t){
-	for(int j=1;j<=5;j++)
-		if(cnt[j].company==c)
+void record(string c, string t)
+{
+	for (int j = 1; j <= 5; j++)
+		if (cnt[j].company == c)
 		{
-			cnt[j].time[cnt[j].i]=t;
-			cnt[j].i++;			
+			cnt[j].time[cnt[j].i] = t;
+			cnt[j].i++;
 			break;
 		}
-	
-		
 }
 
-
-void password_out() {
+void password_out()
+{
 	ofstream outfile;
 	outfile.open("password.out");
 	for (int i = 1; i <= H; i++)
@@ -173,65 +182,87 @@ void password_out() {
 	outfile.close();
 }
 
-void cnt_use_time(int a,int b){
+void cnt_in_time(int a, int b, int T)
+{
 	time_t nowtime;
-	time(&nowtime); //获取1970年1月1日0点0分0秒到现在经过的秒数
-	tm* p = localtime(&nowtime); //将秒数转换为本地时间,年从1900算起,需要+1900,月为0-11,所以要+1
-	win[a][b].use_date[(p->tm_mon + 1)*100+(p->tm_mday)]++;
-
+	time(&nowtime);				 
+	tm *p = localtime(&nowtime); 
+	int h = (p->tm_hour);
+	int m = (p->tm_min);
+	int s = (p->tm_sec);
+	win[a][b].use_date[T].use_t[win[a][b].use_date[T].use_time].in_hour = h;
+	win[a][b].use_date[T].use_t[win[a][b].use_date[T].use_time].in_minute = m;
+	win[a][b].use_date[T].use_t[win[a][b].use_date[T].use_time].in_sec = s;
+	system("pause");
 }
 
-void put_in()
+void cnt_out_time(int a, int b, int T)
+{
+	time_t nowtime;
+	time(&nowtime);				 
+	tm *p = localtime(&nowtime); 
+
+	int h = (p->tm_hour);
+	int m = (p->tm_min);
+	int s = (p->tm_sec);
+	win[a][b].use_date[T].use_time++;
+	win[a][b].use_date[T].use_t[win[a][b].use_date[T].use_time].out_hour = h;
+	win[a][b].use_date[T].use_t[win[a][b].use_date[T].use_time].out_minute = m;
+	win[a][b].use_date[T].use_t[win[a][b].use_date[T].use_time].out_sec = s;
+}
+
+void put_in(int T)
 {
 	cout << "请选择要存入的窗格号：";
 	int t;
 	cin >> t;
-	if (t > H * W) {
+	if (t > H * W)
+	{
 		cout << "输入错误请重新输入。";
 		Sleep(1000);
-		put_in();
+		put_in(T);
 	}
 
-	int x, y;
+	int a, b;
 	for (int i = 1; i <= H; i++)
 		for (int j = 1; j <= W; j++)
 			if (win[i][j].number == t)
 			{
-				x = i, y = j;
+				a = i, b = j;
 				break;
 			}
-	if (win[x][y].state == false)
+	if (win[a][b].state == false)
 	{
 		cout << "该窗口暂时不可用，请换一个。" << endl;
 		Sleep(1000);
-		put_in();
+		put_in(T);
 	}
-	win[x][y].state = false;
-
-	cnt_use_time(x,y);
-
-	cout << endl << "请输入顾客手机尾号后4位：";
-	cin >> win[x][y].phone_number;
-	while (win[x][y].phone_number < 1000 || win[x][y].phone_number>9999) {
+	win[a][b].state = false;
+	cout << endl
+		 << "请输入顾客手机尾号后4位：";
+	cin >> win[a][b].phone_number;
+	while (win[a][b].phone_number < 1000 || win[a][b].phone_number > 9999)
+	{
 		cout << "手机号错误，请重新输入" << endl;
-		cin >> win[x][y].phone_number;
+		cin >> win[a][b].phone_number;
 	}
-	win[x][y].password = getRand(1000, 9999);
-	cout << endl << "顾客取件码为:" << win[x][y].password;
-	password_out();
-	
+	win[a][b].password = getRand(1000, 9999);
+	cout << endl
+		 << "顾客取件码为:" << win[a][b].password << endl;
 	cout << "存入完成";
+	password_out();
+	cnt_in_time(a, b, T);
 	Sleep(1000);
 	return;
 }
 
-void out_usedate(int a,int b,int d)
+void out_usedate(int a, int b, int d)
 {
-	cout<<"第"<<win[a][b].number<<"格在"<<d/100<<"月"<<d%100<<"日被用了"<<win[a][b].use_date[d]<<"次"<<endl;
+	cout << "第" << win[a][b].number << "格在" << d / 100 << "月" << d % 100 << "日被用了" << win[a][b].use_date[d].use_time << "次" << endl;
 	system("pause");
 }
 
-void delivery_login(int n)
+void delivery_login(int n, int T)
 {
 	system("cls");
 
@@ -239,67 +270,70 @@ void delivery_login(int n)
 	cout << "您的电话号码是" << expm[n].phone_number << endl;
 	cout << "请输入您的登录密码：";
 	time_t now = time(0);
-    char* datetime = ctime(&now);
-	record(expm[n].company,datetime);
+	char *datetime = ctime(&now);
+	record(expm[n].company, datetime);
 	string temp_pass;
 	cin >> temp_pass;
 	if (temp_pass == expm[n].password)
 	{
 		system("cls");
 		print_state();
-		put_in();
+		put_in(T);
 	}
 	else
 	{
 		cout << "密码错误请重新输入。";
 		Sleep(1500);
-		delivery_login(n);
+		delivery_login(n, T);
 	}
 }
 
 void print_cnt()
 {
-	for(int j=1;j<=5;j++)
+	for (int j = 1; j <= 5; j++)
 	{
-		cout<<cnt[j].company<<"使用记录："<<endl;
-		for(int k=1;k<=cnt[j].i;k++)
+		cout << cnt[j].company << "使用记录：" << endl;
+		for (int k = 1; k <= cnt[j].i; k++)
 		{
-			cout<<cnt[j].time[k]<<endl;
+			cout << cnt[j].time[k] << endl;
 		}
 	}
 	system("pause");
 }
 
-void get_out(int x, int y)
+void get_out(int x, int y, int T)
 {
-	cout << endl << "您的包裹在" << win[x][y].number << "号窗口，请输入取件码:";
+	cout << endl
+		 << "您的包裹在" << win[x][y].number << "号窗口，请输入取件码:";
 	int temp;
 	cin >> temp;
-	if (temp != win[x][y].password)
+	while (temp != win[x][y].password)
 	{
-		cout << endl << "密码错误请重新输入:";
+		cout << endl;
+		cout << "密码错误请重新输入:";
 		Sleep(1000);
-		get_out(x, y);
+		cin >> temp;
 	}
-	else
-	{
-		win[x][y].state = true;
-		win[x][y].phone_number = 0;
-		win[x][y].password = 0;
-		password_out();
-		cout << endl << "请取走您的包裹";
-		Sleep(1000);
-		return;
-	}
+	win[x][y].state = true;
+	win[x][y].phone_number = 0;
+	win[x][y].password = 0;
+	password_out();
+	cout << endl;
+	cout << "请取走您的包裹";
+	cnt_out_time(x, y, T);
+	Sleep(1000);
+	return;
 }
 
-void customer()
+void customer(int T)
 {
+
 	system("cls");
 	cout << "您好,取件请输入手机尾号后4位：";
 	int num;
 	cin >> num;
-	while (num < 1000 || num>9999) {
+	while (num < 1000 || num > 9999)
+	{
 		cin >> num;
 		cout << "手机号错误，请重新输入" << endl;
 	}
@@ -318,58 +352,127 @@ void customer()
 	}
 	if (flag == false)
 	{
-		cout << endl << "未查询到您的包裹，请重新输入";
+		cout << endl
+			 << "未查询到您的包裹，请重新输入";
 		Sleep(1000);
-		customer();
+		customer(T);
 	}
-	get_out(l, k);
+	get_out(l, k, T);
+}
+
+int cnt_time(int x, int y, int T, int i)
+{
+	int a = win[x][y].use_date[T].use_t[i].in_hour * 3600.0 + win[x][y].use_date[T].use_t[i].in_minute * 60.0 + win[x][y].use_date[T].use_t[i].in_sec;
+	int b = win[x][y].use_date[T].use_t[i].out_hour * 3600.0 + win[x][y].use_date[T].use_t[i].out_minute * 60.0 + win[x][y].use_date[T].use_t[i].out_sec;
+	int c;
+	c = b - a;
+	return c;
+}
+
+void out_use_time(int date, int x, int y)
+{
+	int average = 0;
+	for (int i = 1; i <= win[x][y].use_date[date].use_time; i++)
+		average += cnt_time(x, y, date, i);
+
+	average = average / (win[x][y].use_date[date].use_time * 1.0);
+	int h, m, s;
+	h = average / 3600;
+	m = average % 3600 / 60;
+	s = average % 60;
+	cout << "在" << date / 100 << "月" << date % 100 << "日第" << x << "行" << y << "列平均使用了" << h << "小时" << m << "分" << s << "秒" << endl;
+	system("pause");
+}
+
+void data()
+{
+	cnt[1].time[1]="Sat Apr 25 18:07:52 2024";
+	cnt[2].time[1]="Sat Apr 06 12:27:32 2024";
+	cnt[2].time[2]="Sun Feb 15 18:11:43 2024";
+	cnt[3].time[1]="Sun Apr 01 12:04:12 2024";
+	cnt[3].time[1]="Sun Apr 01 13:01:34 2024";
+	cnt[4].time[1]="Thu Jan 11 11:03:52 2024";
+	cnt[5].time[1]="Wed Mar 27 02:33:12 2024";
+	win[1][2].use_date[425].use_time=1;
+	win[2][3].use_date[406].use_time=1;
+	win[3][1].use_date[215].use_time=1;
+	win[4][4].use_date[401].use_time=2;
+	win[1][2].use_date[117].use_time=1;
+	win[1][2].use_date[327].use_time=1;
+
+	win[1][2].use_date[425].use_t[1].in_hour = 18;
+	win[1][2].use_date[425].use_t[1].in_minute = 07;
+	win[1][2].use_date[425].use_t[1].in_sec = 52;
+	win[1][2].use_date[425].use_t[1].out_hour = 19;
+	win[1][2].use_date[425].use_t[1].out_minute = 41;
+	win[1][2].use_date[425].use_t[1].out_sec = 14;
+	
+	win[2][3].use_date[406].use_t[1].in_hour = 12;
+	win[2][3].use_date[406].use_t[1].in_minute = 27;
+	win[2][3].use_date[406].use_t[1].in_sec = 52;
+	win[2][3].use_date[406].use_t[1].out_hour = 19;
+	win[2][3].use_date[406].use_t[1].out_minute = 41;
+	win[2][3].use_date[406].use_t[1].out_sec = 14;
+	
+	
 }
 
 void login()
 {
+	
 	system("cls");
-	bool flag = false;
 
 	cout << "时间:";
 	time_t t = time(nullptr);
-	tm* now = localtime(&t);
+	tm *now = localtime(&t);
+	time_t nowtime;
+	time(&nowtime);
+	tm *p = localtime(&nowtime);
+	int T = (p->tm_mon + 1) * 100 + (p->tm_mday);
+
 	cout << put_time(now, "%Y-%m-%d %H:%M:%S") << endl;
 	cout << "欢迎使用外卖柜" << endl;
 	cout << "当前使用状态：" << endl;
 	print_state();
-	cout << "顾客按1，骑手按2,查询快递公司使用记录按3，查询快递柜使用记录按4" << endl;
-	int identity;
-	cin >> identity;
+	cout << "顾客按1，骑手按2,查询快递公司使用记录按3，查询快递柜使用记录按4,查询指定日期从快递投放到用户取件的平均时长按5" << endl;
 
+	bool flag = false;
+	int identity;
+
+	cin >> identity;
 	if (identity == 2)
 	{
 		flag = true;
 		cout << "请输入您的快递员编号:";
-		int t;
-		do {
-			cin >> t;
+		int x;
+		do
+		{
+			cin >> x;
 			cout << "快递员编号错误，请重新输入" << endl;
-		} while (t >= N);
-		delivery_login(t);
+		} while (x >= N);
+		delivery_login(x, T);
 		return;
 	}
 	if (identity == 1)
 	{
 		flag = true;
-		customer();
+		customer(T);
 		return;
 	}
 	if (identity == 3)
 	{
-		flag = true;
-		print_cnt();
+		if (identity == 3)
+		{
+			flag = true;
+			print_cnt();
+		}
 	}
-	if(identity == 4)
+	if (identity == 4)
 	{
 		flag = true;
-		cout<<"请输入需要查询窗口号和日期";		
-		int a,b,d,t;
-		cin>>t>>d;
+		cout << "请输入需要查询窗口号和日期";
+		int a, b, d, t;
+		cin >> t >> d;
 		for (int i = 1; i <= H; i++)
 			for (int j = 1; j <= W; j++)
 				if (win[i][j].number == t)
@@ -377,12 +480,29 @@ void login()
 					a = i, b = j;
 					break;
 				}
-		while(a > H||b>W||d<=0||d>=9999) {
+		while (a > H || b > W || d <= 0 || d >= 9999)
+		{
 			cout << "输入错误请重新输入。";
 			Sleep(1000);
-			cin>>a>>b>>d;
+			cin >> a >> b >> d;
 		}
-		out_usedate(a,b,d);
+		out_usedate(a, b, d);
+	}
+	if (identity == 5)
+	{
+		flag = true;
+		cout << "请输入需要查询窗口号和日期" << endl;
+		int x, a, b, d;
+		cin >> x >> d;
+		for (int i = 1; i <= H; i++)
+			for (int j = 1; j <= W; j++)
+				if (win[i][j].number == x)
+				{
+					a = i, b = j;
+					break;
+				}
+
+		out_use_time(d, a, b);
 	}
 	if (flag == false)
 	{
@@ -391,14 +511,18 @@ void login()
 		system("cls");
 		login();
 	}
-
 }
 
-
-
-int	 main()
+int main()
 {
 	initialize();
+	cout<<"是否导入模拟初始数据,y/n";
+	char cha;
+	cin>>cha;
+	if(cha=='y')
+	{
+		data();
+	}
 	while (1)
 		login();
 	return 0;
